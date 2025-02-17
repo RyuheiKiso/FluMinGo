c3c69103397f0d3086ed9dc458bc440322aeb562
kiso ryuhei <uw2374@icloud.com>
2025-02-17 23:19:23 +0900

    BasePageクラスにエラー表示メソッドを追加し、ウィジェットの構造を改善。タイトルとボディのプロパティを追加。

--- flutter_app/lib/util/base_page/base_page.dart
+++ flutter_app/lib/util/base_page/base_page.dart
@@ -3,21 +3,29 @@ import '../error_handling/error_handler.dart';
 
 // ベースページクラス
 class BasePage extends StatelessWidget {
+  // タイトル
   final String title;
+  // ボディ
   final Widget body;
 
+  // コンストラクタ
   const BasePage({super.key, required this.title, required this.body});
 
+  // エラーを表示するメソッド
   void showError(BuildContext context, String message) {
     ErrorHandler.showError(context, message);
   }
 
   @override
+  // ウィジェットを構築するメソッド
   Widget build(BuildContext context) {
     return Scaffold(
+      // アプリバー
       appBar: AppBar(
+        // タイトルを設定
         title: Text(title),
       ),
+      // ボディを設定
       body: body,
     );
   }
