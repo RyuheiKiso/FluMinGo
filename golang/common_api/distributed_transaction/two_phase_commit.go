f81084be154e74213cade15bfc7bae022e888627
kiso ryuhei <uw2374@icloud.com>
2025-02-23 23:36:57 +0900

    common_apiモジュールに二相コミットによるデータ一貫性保証機能を追加し、トランザクションの準備、コミット、ロールバックのメソッドを実装

---
+++ golang/common_api/distributed_transaction/two_phase_commit.go
@@ -0,0 +1,47 @@
+package distributed_transaction
+
+import (
+	"context"
+	"log"
+)
+
+// 二相コミットによるデータ一貫性保証
+// 各トランザクションの準備とコミットを管理
+
+type TwoPhaseCommit struct {
+	participants []Participant
+}
+
+type Participant interface {
+	Prepare(ctx context.Context) error
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context) error
+}
+
+func NewTwoPhaseCommit(participants []Participant) *TwoPhaseCommit {
+	return &TwoPhaseCommit{participants: participants}
+}
+
+func (t *TwoPhaseCommit) Execute(ctx context.Context) error {
+	for _, p := range t.participants {
+		if err := p.Prepare(ctx); err != nil {
+			t.rollback(ctx)
+			return err
+		}
+	}
+	for _, p := range t.participants {
+		if err := p.Commit(ctx); err != nil {
+			t.rollback(ctx)
+			return err
+		}
+	}
+	return nil
+}
+
+func (t *TwoPhaseCommit) rollback(ctx context.Context) {
+	for _, p := range t.participants {
+		if err := p.Rollback(ctx); err != nil {
+			log.Printf("Rollback failed: %v", err)
+		}
+	}
+}
