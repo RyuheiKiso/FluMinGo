b026295e8d55a04b9a0acbf558131f8c3838bc82
kiso ryuhei <uw2374@icloud.com>
2025-02-23 23:36:40 +0900

    common_apiモジュールに依存性注入コンテナ機能を追加し、サービスの登録と解決のメソッドを実装

---
+++ golang/common_api/di/container.go
@@ -0,0 +1,30 @@
+package di
+
+import (
+	"sync"
+)
+
+// DIコンテナの初期化と依存関係登録
+
+type Container struct {
+	services map[string]interface{}
+	mutex    sync.Mutex
+}
+
+func NewContainer() *Container {
+	return &Container{services: make(map[string]interface{})}
+}
+
+func (c *Container) Register(name string, service interface{}) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.services[name] = service
+}
+
+func (c *Container) Resolve(name string) interface{} {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	return c.services[name]
+}
