fbba9ae3bedcbbe318b2ffd91ac355cb1372e0c2
kiso ryuhei <uw2374@icloud.com>
2025-02-24 00:13:32 +0900

    common_apiモジュールに認証管理機能を追加

---
+++ golang/common_api/common/auth_manager.go
@@ -0,0 +1,47 @@
+package common
+
+import (
+	"errors"
+	"sync"
+)
+
+// AuthManager は認証・認可を管理する構造体です。
+type AuthManager struct {
+	users map[string]string // ユーザー名とパスワードのマップ
+	mutex sync.RWMutex
+}
+
+// NewAuthManager は新しい AuthManager インスタンスを作成します。
+func NewAuthManager() *AuthManager {
+	return &AuthManager{
+		users: make(map[string]string),
+	}
+}
+
+// RegisterUser は新しいユーザーを登録します。
+func (am *AuthManager) RegisterUser(username, password string) error {
+	am.mutex.Lock()
+	defer am.mutex.Unlock()
+	if _, exists := am.users[username]; exists {
+		return errors.New("ユーザーは既に存在します")
+	}
+	am.users[username] = password
+	return nil
+}
+
+// Authenticate はユーザーの認証を行います。
+func (am *AuthManager) Authenticate(username, password string) bool {
+	am.mutex.RLock()
+	defer am.mutex.RUnlock()
+	storedPassword, exists := am.users[username]
+	if !exists || storedPassword != password {
+		return false
+	}
+	return true
+}
+
+// Authorize はユーザーの認可を行います。
+func (am *AuthManager) Authorize(username, resource string) bool {
+	// 簡単な例として、全てのユーザーに全てのリソースへのアクセスを許可します。
+	return true
+}
