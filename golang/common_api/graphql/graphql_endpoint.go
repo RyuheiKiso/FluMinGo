d5abe12df957e5cc2423adfc1aa7cd068e85c593
kiso ryuhei <uw2374@icloud.com>
2025-02-24 21:48:59 +0900

    common_apiモジュールにGraphQLスキーマにバージョン情報、サーバーのステータス、および稼働時間を追加する機能を実装

--- golang/common_api/graphql/graphql_endpoint.go
+++ golang/common_api/graphql/graphql_endpoint.go
@@ -3,6 +3,7 @@ package graphql
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/graphql-go/graphql"
 )
@@ -24,6 +25,15 @@ func GraphQLEndpoint(w http.ResponseWriter, r *http.Request) {
 		}),
 	})
 
+	// 新しいフィールドを追加して、GraphQLスキーマにバージョン情報を追加します。
+	addVersionField(&schema)
+
+	// 新しいフィールドを追加して、GraphQLスキーマにサーバーのステータスを追加します。
+	addStatusField(&schema)
+
+	// 新しいフィールドを追加して、GraphQLスキーマにサーバーの稼働時間を追加します。
+	addUptimeField(&schema, time.Now())
+
 	// クエリのパース
 	result := graphql.Do(graphql.Params{
 		Schema:        schema,
@@ -33,4 +43,37 @@ func GraphQLEndpoint(w http.ResponseWriter, r *http.Request) {
 	// 結果の返却
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(result)
+}
+
+// 新しいフィールドを追加して、GraphQLスキーマにバージョン情報を追加します。
+func addVersionField(schema *graphql.Schema) {
+	versionField := &graphql.Field{
+		Type: graphql.String,
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return "1.0.0", nil
+		},
+	}
+	schema.QueryType().AddFieldConfig("version", versionField)
+}
+
+// 新しいフィールドを追加して、GraphQLスキーマにサーバーのステータスを追加します。
+func addStatusField(schema *graphql.Schema) {
+	statusField := &graphql.Field{
+		Type: graphql.String,
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return "Server is running", nil
+		},
+	}
+	schema.QueryType().AddFieldConfig("status", statusField)
+}
+
+// 新しいフィールドを追加して、GraphQLスキーマにサーバーの稼働時間を追加します。
+func addUptimeField(schema *graphql.Schema, startTime time.Time) {
+	uptimeField := &graphql.Field{
+		Type: graphql.String,
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return time.Since(startTime).String(), nil
+		},
+	}
+	schema.QueryType().AddFieldConfig("uptime", uptimeField)
 }
