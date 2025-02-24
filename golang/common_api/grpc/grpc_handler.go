ce35a84139423ab87e595a63896c511534fdaa88
kiso ryuhei <uw2374@icloud.com>
2025-02-24 21:49:03 +0900

    common_apiモジュールにgRPCサーバーのステータス、バージョン、および稼働時間を取得するメソッドを追加

--- golang/common_api/grpc/grpc_handler.go
+++ golang/common_api/grpc/grpc_handler.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -40,4 +41,19 @@ func (s *GRPCServer) Start(address string) error {
 func (s *GRPCServer) Stop() {
 	log.Println("gRPCサーバーを停止します...")
 	s.server.GracefulStop()
+}
+
+// 新しいメソッドを追加して、サーバーのステータスを取得します。
+func (s *GRPCServer) GetStatus() string {
+	return "Server is running"
+}
+
+// 新しいメソッドを追加して、サーバーのバージョンを取得します。
+func (s *GRPCServer) GetVersion() string {
+	return "1.0.0"
+}
+
+// 新しいメソッドを追加して、サーバーの稼働時間を取得します。
+func (s *GRPCServer) GetUptime(startTime time.Time) time.Duration {
+	return time.Since(startTime)
 }
