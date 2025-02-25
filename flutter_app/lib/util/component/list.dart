72a1f17b0fdef19ee1da45633162b61e4e5e31b7
kiso ryuhei <uw2374@icloud.com>
2025-02-25 22:18:19 +0900

    ListComponentにパディング、スクロール方向、スクロールの反転プロパティを追加

--- flutter_app/lib/util/component/list.dart
+++ flutter_app/lib/util/component/list.dart
@@ -4,12 +4,27 @@ import 'package:flutter/material.dart';
 class ListComponent extends StatelessWidget {
   // リストのアイテム
   final List<Widget> items;
+  // パディング
+  final EdgeInsetsGeometry? padding;
+  // スクロール方向
+  final Axis scrollDirection;
+  // スクロールの反転
+  final bool reverse;
 
-  const ListComponent({super.key, required this.items});
+  const ListComponent({
+    super.key,
+    required this.items,
+    this.padding,
+    this.scrollDirection = Axis.vertical,
+    this.reverse = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListView(
+      padding: padding,
+      scrollDirection: scrollDirection,
+      reverse: reverse,
       children: items,
     );
   }
