59e14cd2edf1094e61c19961abc2e2a85ac5f22e
kiso ryuhei <uw2374@icloud.com>
2025-02-25 22:12:45 +0900

    GridComponentにパディング、スペーシング、スクロールの収縮プロパティを追加

--- flutter_app/lib/util/component/grid.dart
+++ flutter_app/lib/util/component/grid.dart
@@ -6,14 +6,33 @@ class GridComponent extends StatelessWidget {
   final List<Widget> items;
   // クロスアクシスのカウント
   final int crossAxisCount;
+  // パディング
+  final EdgeInsetsGeometry? padding;
+  // スペーシング
+  final double spacing;
+  // スクロールの収縮
+  final bool shrinkWrap;
 
-  const GridComponent({super.key, required this.items, required this.crossAxisCount});
+  const GridComponent({
+    super.key,
+    required this.items,
+    required this.crossAxisCount,
+    this.padding,
+    this.spacing = 8.0,
+    this.shrinkWrap = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: crossAxisCount,
-      children: items,
+    return Padding(
+      padding: padding ?? EdgeInsets.all(0),
+      child: GridView.count(
+        crossAxisCount: crossAxisCount,
+        children: items,
+        crossAxisSpacing: spacing,
+        mainAxisSpacing: spacing,
+        shrinkWrap: shrinkWrap,
+      ),
     );
   }
 }
