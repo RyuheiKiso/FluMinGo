86aa21a869ec196dd17712246fefec0f23697892
kiso ryuhei <uw2374@icloud.com>
2025-02-25 22:12:48 +0900

    LabelComponentにテキストの配置、最大行数、オーバーフロー処理のプロパティを追加

--- flutter_app/lib/util/component/label.dart
+++ flutter_app/lib/util/component/label.dart
@@ -6,14 +6,30 @@ class LabelComponent extends StatelessWidget {
   final String text;
   // テキストスタイル
   final TextStyle? style;
+  // テキストの配置
+  final TextAlign? alignment;
+  // 最大行数
+  final int? maxLines;
+  // オーバーフローの処理
+  final TextOverflow? overflow;
 
-  const LabelComponent({super.key, required this.text, this.style});
+  const LabelComponent({
+    super.key,
+    required this.text,
+    this.style,
+    this.alignment,
+    this.maxLines,
+    this.overflow,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       style: style,
+      textAlign: alignment,
+      maxLines: maxLines,
+      overflow: overflow,
     );
   }
 }
