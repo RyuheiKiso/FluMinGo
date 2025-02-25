b097472b080899f157e00f3c676c949c2ece76b2
kiso ryuhei <uw2374@icloud.com>
2025-02-25 22:23:03 +0900

    ListViewComponentに背景色、分割線表示、スクロールバー表示のプロパティを追加

--- flutter_app/lib/util/component/list_view.dart
+++ flutter_app/lib/util/component/list_view.dart
@@ -14,6 +14,12 @@ class ListViewComponent extends StatelessWidget {
   final bool reverse;
   // スクロールコントローラー
   final ScrollController? controller;
+  // リストの背景色
+  final Color? backgroundColor;
+  // リストの分割線
+  final bool showDividers;
+  // リストのスクロールバーの表示
+  final bool showScrollbar;
 
   // コンストラクタにpaddingとspacingを追加
   const ListViewComponent({
@@ -24,24 +30,41 @@ class ListViewComponent extends StatelessWidget {
     this.scrollDirection = Axis.vertical,
     this.reverse = false,
     this.controller,
+    this.backgroundColor,
+    this.showDividers = false,
+    this.showScrollbar = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(padding),
-      child: ListView.separated(
+    Widget listView = ListView.separated(
+      itemCount: items.length,
+      scrollDirection: scrollDirection,
+      reverse: reverse,
+      controller: controller,
+      itemBuilder: (context, index) {
+        return items[index];
+      },
+      separatorBuilder: (context, index) {
+        return SizedBox(height: spacing);
+      },
+    );
+
+    if (showDividers) {
+      listView = ListView.separated(
         itemCount: items.length,
-        scrollDirection: scrollDirection,
-        reverse: reverse,
-        controller: controller,
-        itemBuilder: (context, index) {
-          return items[index];
-        },
-        separatorBuilder: (context, index) {
-          return SizedBox(height: spacing);
-        },
-      ),
+        itemBuilder: (context, index) => items[index],
+        separatorBuilder: (context, index) => Divider(),
+      );
+    }
+
+    if (showScrollbar) {
+      listView = Scrollbar(child: listView);
+    }
+
+    return Container(
+      color: backgroundColor,
+      child: listView,
     );
   }
 }
