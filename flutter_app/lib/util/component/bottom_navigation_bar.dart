d72bc6dc3dc5e44d65107dd19359a02f456adf25
kiso ryuhei <uw2374@icloud.com>
2025-02-25 21:53:35 +0900

    BottomNavigationBarComponentにアイコンサイズとフォントサイズの新しいプロパティを追加

--- flutter_app/lib/util/component/bottom_navigation_bar.dart
+++ flutter_app/lib/util/component/bottom_navigation_bar.dart
@@ -24,6 +24,9 @@ import 'package:flutter/material.dart';
 ///   selectedItemColor: Colors.white, // 選択されたアイテムの色を追加
 ///   unselectedItemColor: Colors.grey, // 選択されていないアイテムの色を追加
 ///   showLabels: true, // ラベルの表示を追加
+///   iconSize: 24.0, // アイコンサイズを追加
+///   selectedFontSize: 14.0, // 選択されたフォントサイズを追加
+///   unselectedFontSize: 12.0, // 選択されていないフォントサイズを追加
 /// );
 /// ```
 class BottomNavigationBarComponent extends StatelessWidget {
@@ -41,6 +44,12 @@ class BottomNavigationBarComponent extends StatelessWidget {
   final Color? unselectedItemColor; // 新しいプロパティ
   // ラベルの表示
   final bool showLabels; // 新しいプロパティ
+  // アイコンサイズ
+  final double? iconSize; // 新しいプロパティ
+  // 選択されたフォントサイズ
+  final double? selectedFontSize; // 新しいプロパティ
+  // 選択されていないフォントサイズ
+  final double? unselectedFontSize; // 新しいプロパティ
 
   /// コンストラクタ
   /// 
@@ -51,6 +60,9 @@ class BottomNavigationBarComponent extends StatelessWidget {
   /// [selectedItemColor] 選択されたアイテムの色
   /// [unselectedItemColor] 選択されていないアイテムの色
   /// [showLabels] ラベルの表示
+  /// [iconSize] アイコンサイズ
+  /// [selectedFontSize] 選択されたフォントサイズ
+  /// [unselectedFontSize] 選択されていないフォントサイズ
   const BottomNavigationBarComponent({
     super.key,
     required this.currentIndex,
@@ -60,6 +72,9 @@ class BottomNavigationBarComponent extends StatelessWidget {
     this.selectedItemColor,
     this.unselectedItemColor,
     this.showLabels = true,
+    this.iconSize,
+    this.selectedFontSize,
+    this.unselectedFontSize,
   });
 
   @override
@@ -81,6 +96,12 @@ class BottomNavigationBarComponent extends StatelessWidget {
       // ラベルの表示
       showUnselectedLabels: showLabels,
       showSelectedLabels: showLabels,
+      // アイコンサイズ
+      iconSize: iconSize ?? 24.0,
+      // 選択されたフォントサイズ
+      selectedFontSize: selectedFontSize ?? 14.0,
+      // 選択されていないフォントサイズ
+      unselectedFontSize: unselectedFontSize ?? 12.0,
     );
   }
 }
