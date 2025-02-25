d91787d3bdeb69617b2d57096f438ed3b8e5a891
kiso ryuhei <uw2374@icloud.com>
2025-02-25 23:54:33 +0900

    DataConversionManagerのユニットテストを追加: JSONとMapの変換、日付文字列とDateTimeの変換テストケースを実装

--- flutter_app/test/util/data_conversion_manager_test.dart
+++ flutter_app/test/util/data_conversion_manager_test.dart
@@ -3,8 +3,32 @@ import 'package:flutter_app/util/data_conversion/data_conversion_manager.dart';
 
 void main() {
   group('DataConversionManager Tests', () {
-    test('Example test', () {
-      // Add your test cases here
+    test('jsonToMap converts JSON string to Map', () {
+      final manager = DataConversionManager();
+      final jsonString = '{"key": "value"}';
+      final result = manager.jsonToMap(jsonString);
+      expect(result, {'key': 'value'});
+    });
+
+    test('mapToJson converts Map to JSON string', () {
+      final manager = DataConversionManager();
+      final map = {'key': 'value'};
+      final result = manager.mapToJson(map);
+      expect(result, '{"key":"value"}');
+    });
+
+    test('stringToDate converts date string to DateTime', () {
+      final manager = DataConversionManager();
+      final dateString = '2023-10-10T10:00:00.000Z';
+      final result = manager.stringToDate(dateString);
+      expect(result, DateTime.parse(dateString));
+    });
+
+    test('dateToString converts DateTime to date string', () {
+      final manager = DataConversionManager();
+      final date = DateTime.parse('2023-10-10T10:00:00.000Z');
+      final result = manager.dateToString(date);
+      expect(result, '2023-10-10T10:00:00.000Z');
     });
   });
 }
