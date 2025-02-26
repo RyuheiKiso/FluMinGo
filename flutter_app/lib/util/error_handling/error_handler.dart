fc99d11cd357a8e746e3bed09e852a350b63a590
kiso ryuhei <uw2374@icloud.com>
2025-02-26 20:33:56 +0900

    YamlMapの変換メソッドを追加し、設定ファイルの読み込み処理を改善

--- flutter_app/lib/util/error_handling/error_handler.dart
+++ flutter_app/lib/util/error_handling/error_handler.dart
@@ -10,20 +10,35 @@ import 'package:yaml/yaml.dart';
 //    await ErrorHandler.initialize();
 // 2. エラーが発生した場合に ErrorHandler.showError('エラーメッセージ') を呼び出します。
 //    ErrorHandler.showError('エラーメッセージ');
+Map<String, dynamic> _convertYamlMapToMap(YamlMap yamlMap) {
+  final map = <String, dynamic>{};
+  yamlMap.forEach((key, value) {
+    if (value is YamlMap) {
+      map[key] = _convertYamlMapToMap(value);
+    } else if (value is YamlList) {
+      map[key] = value.map((e) => e is YamlMap ? _convertYamlMapToMap(e) : e).toList();
+    } else {
+      map[key] = value;
+    }
+  });
+  return map;
+}
+
 class ErrorHandler {
   static late String logFilePath;
 
   // 初期化メソッド
   static Future<void> initialize() async {
     final config = await _loadConfig();
-    logFilePath = config['log_file_path'] ?? 'error_log.txt';
+    logFilePath = config['log_file_path'] ?? 'test_log.txt';
   }
 
   // 設定ファイルを読み込むメソッド
   static Future<Map<String, dynamic>> _loadConfig() async {
     final file = File('config.yml'); // 相対パスに変更
     final content = await file.readAsString();
-    return loadYaml(content);
+    final yamlMap = loadYaml(content) as YamlMap;
+    return _convertYamlMapToMap(yamlMap);
   }
 
   // エラーメッセージを表示するメソッド
