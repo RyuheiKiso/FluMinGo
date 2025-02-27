fe241b00ea62c6c0be9b07c1e763473398d7eedb
kiso ryuhei <uw2374@icloud.com>
2025-02-27 22:42:52 +0900

    GraphQLServiceのテストを更新: インスタンス化を削除し、静的メソッドを使用するように変更

--- flutter_app/test/util/graphql_service_test.dart
+++ flutter_app/test/util/graphql_service_test.dart
@@ -2,24 +2,23 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_app/util/graphql_service.dart';
+import 'package:flutter_app/util/graphql/graphql_service.dart';
 import 'graphql_service_test.mocks.dart';
 
 @GenerateMocks([GraphQLClient])
 void main() {
   late MockGraphQLClient mockGraphQLClient;
-  late GraphQLService graphQLService;
 
   setUp(() {
     mockGraphQLClient = MockGraphQLClient();
-    graphQLService = GraphQLService(graphQLClient: mockGraphQLClient);
+    // No need to instantiate GraphQLService
   });
 
   test('Test GraphQLService query', () async {
     final options = QueryOptions(document: gql('query { test }'));
     when(mockGraphQLClient.query(options)).thenAnswer((_) async => QueryResult(data: {'test': 'value'}, options: options, source: QueryResultSource.network));
 
-    final result = await graphQLService.query(options);
+    final result = await GraphQLService.performQuery('query { test }');
 
     expect(result.data, {'test': 'value'});
   });
@@ -28,7 +27,7 @@ void main() {
     final options = MutationOptions(document: gql('mutation { test }'));
     when(mockGraphQLClient.mutate(options)).thenAnswer((_) async => QueryResult(data: {'test': 'value'}, options: options, source: QueryResultSource.network));
 
-    final result = await graphQLService.mutate(options);
+    final result = await GraphQLService.performMutation('mutation { test }');
 
     expect(result.data, {'test': 'value'});
   });
